298ecdcf125d069125f1812a2d6ee31891e79c62
Mani Sundararajan <mani@hey.com>
2023-10-21 19:54:46 -0700

    parse input char by char

--- src/main.rs
+++ src/main.rs
@@ -2,43 +2,112 @@ use std::env;
 use std::io;
 use std::process;
 
+// fn match_here(input_iter: Iter<&str>, pattern: Iter<&str>) -> bool {
+//     true
+// }
+//
+#[derive(Debug)]
+enum Pattern {
+    Digit,
+    Alphanumeric,
+    PositiveGroup(String),
+    NegativeGroup(String),
+    Normal(char),
+}
+
+fn parse_pattern(pattern: &str) -> Vec<Pattern> {
+    let patt_chars = pattern.chars().collect::<Vec<char>>();
+
+    let mut patt_vec: Vec<Pattern> = Vec::new();
+    let mut patt_pos = 0;
+    while patt_pos < patt_chars.len() {
+        // check if we are evaluating a pattern
+        let cur_pattern;
+        let cur_char = patt_chars.get(patt_pos).unwrap();
+        if cur_char.eq(&'\\') {
+            match patt_chars.get(patt_pos + 1) {
+                Some(c) => {
+                    cur_pattern = if let 'd' = c {
+                        Pattern::Digit
+                    } else {
+                        Pattern::Alphanumeric
+                    };
+                    patt_pos += 2;
+                }
+                None => panic!("Unhandled Pattern"),
+            }
+            patt_vec.push(cur_pattern);
+        } else if cur_char.eq(&'[') {
+            // Look for char groups
+            let pos_end_group = patt_chars
+                .iter()
+                .position(|&x| x == ']')
+                .expect("Unhandled Pattern");
+            let inside_group: String = patt_chars[patt_pos + 1..pos_end_group].iter().collect();
+            cur_pattern = if inside_group.starts_with('^') {
+                Pattern::NegativeGroup(String::from(
+                    inside_group
+                        .strip_prefix('^')
+                        .expect("pattern should be starting with a ^"),
+                ))
+            } else {
+                Pattern::PositiveGroup(String::from(inside_group))
+            };
+            patt_vec.push(cur_pattern);
+
+            patt_pos = pos_end_group + 1;
+        } else {
+            cur_pattern = Pattern::Normal(*cur_char);
+            patt_vec.push(cur_pattern);
+            patt_pos += 1;
+        };
+    }
+
+    patt_vec
+}
+
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
+        // Single Char matching
         return input_line.contains(pattern);
     } else {
-        // Match on different character classes
-        match pattern {
-            "\\d" => input_line.chars().filter(|c| c.is_digit(10)).count() > 0,
-            "\\w" => {
-                input_line
-                    .chars()
-                    .filter(|c| c.is_ascii_alphanumeric())
-                    .count()
-                    > 0
-            }
-            _ => {
-                if pattern.starts_with('[') && pattern.ends_with(']') {
-                    let inside_group: Vec<_> = pattern
-                        .strip_prefix('[')
-                        .expect("Pattern should start with [")
-                        .strip_suffix(']')
-                        .expect("Pattern should end with ]")
-                        .chars()
-                        .collect();
-                    if inside_group.starts_with(&['^']) {
-                        let negative_char_group =
-                            inside_group.strip_prefix(&['^']).expect("Starts with ^");
-                        input_line
-                            .chars()
-                            .all(|c| !negative_char_group.contains(&c))
-                    } else {
-                        input_line.chars().any(|c| inside_group.contains(&c))
-                    }
+        let mut input_chars = input_line.trim().chars().enumerate();
+        let patterns = parse_pattern(pattern);
+        println!("{:?}", &patterns);
+
+        let mut patt_idx = 0;
+
+        while let Some((char_idx, cur_char)) = input_chars.next() {
+            if let Some(p) = patterns.get(patt_idx) {
+                let is_match = match p {
+                    Pattern::Digit => cur_char.is_digit(10),
+                    Pattern::Alphanumeric => cur_char.is_alphanumeric(),
+                    Pattern::PositiveGroup(grp) => grp.contains(cur_char),
+                    Pattern::NegativeGroup(grp) => !grp.contains(cur_char),
+                    Pattern::Normal(c) => cur_char.eq(c),
+                };
+
+                if is_match {
+                    // println!("{:?} - {:?} | {:?} - {:?}", cur_char, p, char_idx, patt_idx);
+                    patt_idx += 1;
                 } else {
-                    panic!("Unhandled Pattern");
+                    patt_idx = 0;
                 }
+            } else {
+                if char_idx < input_line.len() {
+                    return true;
+                }
+                patt_idx = 0;
             }
         }
+
+        // println!("{:?}", patt_idx);
+
+        if patt_idx == patterns.len() {
+            return true;
+        }
+
+        false
     }
 }
 
